772e57e67c5d9617f8ca5c6d216b5f8b20015a0a
Omer <omermiz1604gmail.com>
2020-02-26 00:36:42 +0200

    Updates

---
+++ precache-manifest.17701f075303e85a3b6546d18d029f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf7d3a0e1af0a14d4c2f6595d6dbb3f",
+    "url": "/nba/index.html"
+  },
+  {
+    "revision": "a8420dadbb1d881d6847",
+    "url": "/nba/static/css/main.5d402ed1.chunk.css"
+  },
+  {
+    "revision": "ad3df67f923c920e2001",
+    "url": "/nba/static/js/2.e4336d25.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/nba/static/js/2.e4336d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8420dadbb1d881d6847",
+    "url": "/nba/static/js/main.824689aa.chunk.js"
+  },
+  {
+    "revision": "9af04093ecf1ecab1b4e",
+    "url": "/nba/static/js/runtime-main.a5bbf247.js"
+  },
+  {
+    "revision": "8f716421614562343830fe40dd9be8c6",
+    "url": "/nba/static/media/bg.8f716421.jpg"
+  }
+]);
